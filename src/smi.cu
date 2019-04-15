bae5fed1c43e925c90af50da24dc09522f5768d4
Drew Schmidt <wrathematics@gmail.com>
2019-04-15 12:13:16 -0400

    more device queries

--- src/smi.cu
+++ src/smi.cu
@@ -29,8 +29,8 @@ extern "C" SEXP R_smi()
 {
   SEXP ret, ret_names;
   SEXP ret_version;
-  SEXP ret_name, ret_speed, ret_temp, ret_power, ret_power_max;
-  SEXP ret_memory_used, ret_memory_total, ret_utilization;
+  SEXP ret_name, ret_busid, ret_disp;
+  SEXP ret_speed, ret_temp, ret_perf, ret_power, ret_power_max, ret_memory_used, ret_memory_total, ret_utilization, ret_mode;
   
   // init
   CHECK_NVML( nvmlInit() );
@@ -45,13 +45,17 @@ extern "C" SEXP R_smi()
   CHECK_NVML( nvmlDeviceGetCount(&num_gpus) );
   
   PROTECT(ret_name = allocVector(STRSXP, num_gpus));
+  PROTECT(ret_busid = allocVector(STRSXP, num_gpus));
+  PROTECT(ret_disp = allocVector(LGLSXP, num_gpus));
   PROTECT(ret_speed = allocVector(INTSXP, num_gpus));
   PROTECT(ret_temp = allocVector(INTSXP, num_gpus));
+  PROTECT(ret_perf = allocVector(INTSXP, num_gpus));
   PROTECT(ret_power = allocVector(INTSXP, num_gpus));
   PROTECT(ret_power_max = allocVector(INTSXP, num_gpus));
   PROTECT(ret_memory_used = allocVector(REALSXP, num_gpus));
   PROTECT(ret_memory_total = allocVector(REALSXP, num_gpus));
   PROTECT(ret_utilization = allocVector(INTSXP, num_gpus));
+  PROTECT(ret_mode = allocVector(STRSXP, num_gpus));
   
   for (int i=0; i<num_gpus; i++)
   {
@@ -60,6 +64,14 @@ extern "C" SEXP R_smi()
     
     CHECK_NVML( nvmlDeviceGetName(device, str, STRLEN) );
     SET_STRING_ELT(ret_name, i, mkChar(str));
+    
+    nvmlPciInfo_t pci;
+    CHECK_NVML( nvmlDeviceGetPciInfo (device, &pci) );
+    SET_STRING_ELT(ret_busid, i, mkChar(pci.busId));
+    
+    nvmlEnableState_t disp;
+    CHECK_NVML( nvmlDeviceGetDisplayActive(device, &disp) );
+    LOGICAL(ret_disp)[i] = (int) disp;
     
     unsigned int speed;
     CHECK_NVML( nvmlDeviceGetFanSpeed(device, &speed) );
@@ -70,13 +82,17 @@ extern "C" SEXP R_smi()
     CHECK_NVML( nvmlDeviceGetTemperature(device, sensor, &temp) );
     INTEGER(ret_temp)[i] = temp;
     
+    nvmlPstates_t pState;
+    CHECK_NVML( nvmlDeviceGetPerformanceState(device, &pState) );
+    INTEGER(ret_perf)[i] = (int) pState;
+    
     unsigned int power;
     CHECK_NVML( nvmlDeviceGetPowerUsage(device, &power) );
     INTEGER(ret_power)[i] = power;
     
     unsigned int power_min, power_max;
     CHECK_NVML( nvmlDeviceGetPowerManagementLimitConstraints(device, &power_min, &power_max) );
-    INTEGER(ret_power)[i] = power_max;
+    INTEGER(ret_power_max)[i] = power_max;
     
     nvmlMemory_t memory;
     CHECK_NVML( nvmlDeviceGetMemoryInfo(device, &memory) );
@@ -86,11 +102,23 @@ extern "C" SEXP R_smi()
     nvmlUtilization_t utilization;
     CHECK_NVML( nvmlDeviceGetUtilizationRates(device, &utilization) );
     INTEGER(ret_utilization)[i] = utilization.gpu;
+    
+    nvmlComputeMode_t mode;
+    CHECK_NVML( nvmlDeviceGetComputeMode(device, &mode) );
+    if (mode == NVML_COMPUTEMODE_DEFAULT)
+      strcpy(str, "Default");
+    else if (mode == NVML_COMPUTEMODE_EXCLUSIVE_THREAD)
+      strcpy(str, "Exclusive Thread");
+    else if (mode == NVML_COMPUTEMODE_PROHIBITED)
+      strcpy(str, "Prohibited");
+    else if (mode == NVML_COMPUTEMODE_EXCLUSIVE_PROCESS)
+      strcpy(str, "Exclusive Process");
+    SET_STRING_ELT(ret_mode, i, mkChar(str));
   }
   
   CHECK_NVML( nvmlShutdown() );
   
-  int nret = 9;
+  int nret = 13;
   int n = 0;
   PROTECT(ret = allocVector(VECSXP, nret));
   PROTECT(ret_names = allocVector(STRSXP, nret));
@@ -100,10 +128,16 @@ extern "C" SEXP R_smi()
   SET_STRING_ELT(ret_names, n++, mkChar("version"));
   SET_VECTOR_ELT(ret, n, ret_name);
   SET_STRING_ELT(ret_names, n++, mkChar("name"));
+  SET_VECTOR_ELT(ret, n, ret_busid);
+  SET_STRING_ELT(ret_names, n++, mkChar("busid"));
+  SET_VECTOR_ELT(ret, n, ret_disp);
+  SET_STRING_ELT(ret_names, n++, mkChar("disp"));
   SET_VECTOR_ELT(ret, n, ret_speed);
   SET_STRING_ELT(ret_names, n++, mkChar("speed"));
   SET_VECTOR_ELT(ret, n, ret_temp);
   SET_STRING_ELT(ret_names, n++, mkChar("temp"));
+  SET_VECTOR_ELT(ret, n, ret_perf);
+  SET_STRING_ELT(ret_names, n++, mkChar("perf"));
   SET_VECTOR_ELT(ret, n, ret_power);
   SET_STRING_ELT(ret_names, n++, mkChar("power"));
   SET_VECTOR_ELT(ret, n, ret_power_max);
@@ -114,6 +148,8 @@ extern "C" SEXP R_smi()
   SET_STRING_ELT(ret_names, n++, mkChar("memory_total"));
   SET_VECTOR_ELT(ret, n, ret_utilization);
   SET_STRING_ELT(ret_names, n++, mkChar("utilization"));
+  SET_VECTOR_ELT(ret, n, ret_mode);
+  SET_STRING_ELT(ret_names, n++, mkChar("mode"));
   
   UNPROTECT(nret+2);
   return ret;

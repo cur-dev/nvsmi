a0ccc8f8934912be83ccbef0a8c8f1201161c7d5
Drew Schmidt <wrathematics@gmail.com>
2019-04-15 12:16:02 -0400

    simplify interface to nvml

--- src/smi.cu
+++ src/smi.cu
@@ -25,12 +25,91 @@ static inline void check_nvml_ret(nvmlReturn_t check)
 
 
 
+static inline nvmlDevice_t device_get_handle_by_index(int index)
+{
+  nvmlDevice_t device;
+  CHECK_NVML( nvmlDeviceGetHandleByIndex(index, &device) );
+  return device;
+}
+
+static inline void device_get_name(nvmlDevice_t device)
+{
+  CHECK_NVML( nvmlDeviceGetName(device, str, STRLEN) );
+}
+
+static inline int device_get_display_active(nvmlDevice_t device)
+{
+  nvmlEnableState_t disp;
+  CHECK_NVML( nvmlDeviceGetDisplayActive(device, &disp) );
+  return (int) disp;
+}
+
+static inline int device_get_fan_speed(nvmlDevice_t device)
+{
+  unsigned int speed;
+  CHECK_NVML( nvmlDeviceGetFanSpeed(device, &speed) );
+  return (int) speed;
+}
+
+static inline int device_get_temperature(nvmlDevice_t device)
+{
+  nvmlTemperatureSensors_t sensor = NVML_TEMPERATURE_GPU;
+  unsigned int temp;
+  CHECK_NVML( nvmlDeviceGetTemperature(device, sensor, &temp) );
+  return (int) temp;
+}
+
+static inline int device_get_performance_state(nvmlDevice_t device)
+{
+  nvmlPstates_t pState;
+  CHECK_NVML( nvmlDeviceGetPerformanceState(device, &pState) );
+  return (int) pState;
+}
+
+static inline int device_get_power_usage(nvmlDevice_t device)
+{
+  unsigned int power;
+  CHECK_NVML( nvmlDeviceGetPowerUsage(device, &power) );
+  return (int) power;
+}
+
+static inline int device_Get_power_max(nvmlDevice_t device)
+{
+  unsigned int power_min, power_max;
+  CHECK_NVML( nvmlDeviceGetPowerManagementLimitConstraints(device, &power_min, &power_max) );
+  return (int) power_max;
+}
+
+static inline int device_get_utilization(nvmlDevice_t device)
+{
+  nvmlUtilization_t utilization;
+  CHECK_NVML( nvmlDeviceGetUtilizationRates(device, &utilization) );
+  return (int) utilization.gpu;
+}
+
+static inline void device_get_compute_mode(nvmlDevice_t device)
+{
+  nvmlComputeMode_t mode;
+  CHECK_NVML( nvmlDeviceGetComputeMode(device, &mode) );
+  if (mode == NVML_COMPUTEMODE_DEFAULT)
+    strcpy(str, "Default");
+  else if (mode == NVML_COMPUTEMODE_EXCLUSIVE_THREAD)
+    strcpy(str, "Exclusive Thread");
+  else if (mode == NVML_COMPUTEMODE_PROHIBITED)
+    strcpy(str, "Prohibited");
+  else if (mode == NVML_COMPUTEMODE_EXCLUSIVE_PROCESS)
+    strcpy(str, "Exclusive Process");
+}
+
+
+
 extern "C" SEXP R_smi()
 {
   SEXP ret, ret_names;
   SEXP ret_version;
   SEXP ret_name, ret_busid, ret_disp;
-  SEXP ret_speed, ret_temp, ret_perf, ret_power, ret_power_max, ret_memory_used, ret_memory_total, ret_utilization, ret_mode;
+  SEXP ret_speed, ret_temp, ret_perf, ret_power, ret_power_max, ret_memory_used,
+    ret_memory_total, ret_utilization, ret_mode;
   
   // init
   CHECK_NVML( nvmlInit() );
@@ -59,60 +138,35 @@ extern "C" SEXP R_smi()
   
   for (int i=0; i<num_gpus; i++)
   {
-    nvmlDevice_t device;
-    CHECK_NVML( nvmlDeviceGetHandleByIndex(i, &device) );
+    nvmlDevice_t device = device_get_handle_by_index(i);
     
-    CHECK_NVML( nvmlDeviceGetName(device, str, STRLEN) );
+    device_get_name(device);
     SET_STRING_ELT(ret_name, i, mkChar(str));
     
     nvmlPciInfo_t pci;
     CHECK_NVML( nvmlDeviceGetPciInfo (device, &pci) );
     SET_STRING_ELT(ret_busid, i, mkChar(pci.busId));
     
-    nvmlEnableState_t disp;
-    CHECK_NVML( nvmlDeviceGetDisplayActive(device, &disp) );
-    LOGICAL(ret_disp)[i] = (int) disp;
+    LOGICAL(ret_disp)[i] = device_get_display_active(device);
     
-    unsigned int speed;
-    CHECK_NVML( nvmlDeviceGetFanSpeed(device, &speed) );
-    INTEGER(ret_speed)[i] = speed;
+    INTEGER(ret_speed)[i] = device_get_fan_speed(device);
     
-    nvmlTemperatureSensors_t sensor = NVML_TEMPERATURE_GPU;
-    unsigned int temp;
-    CHECK_NVML( nvmlDeviceGetTemperature(device, sensor, &temp) );
-    INTEGER(ret_temp)[i] = temp;
+    INTEGER(ret_temp)[i] = device_get_temperature(device);
     
-    nvmlPstates_t pState;
-    CHECK_NVML( nvmlDeviceGetPerformanceState(device, &pState) );
-    INTEGER(ret_perf)[i] = (int) pState;
+    INTEGER(ret_perf)[i] = device_get_performance_state(device);
     
-    unsigned int power;
-    CHECK_NVML( nvmlDeviceGetPowerUsage(device, &power) );
-    INTEGER(ret_power)[i] = power;
+    INTEGER(ret_power)[i] = device_get_power_usage(device);
     
-    unsigned int power_min, power_max;
-    CHECK_NVML( nvmlDeviceGetPowerManagementLimitConstraints(device, &power_min, &power_max) );
-    INTEGER(ret_power_max)[i] = power_max;
+    INTEGER(ret_power_max)[i] = device_Get_power_max(device);
     
     nvmlMemory_t memory;
     CHECK_NVML( nvmlDeviceGetMemoryInfo(device, &memory) );
     REAL(ret_memory_used)[i] = (double) memory.used;
     REAL(ret_memory_total)[i] = (double) memory.total;
     
-    nvmlUtilization_t utilization;
-    CHECK_NVML( nvmlDeviceGetUtilizationRates(device, &utilization) );
-    INTEGER(ret_utilization)[i] = utilization.gpu;
+    INTEGER(ret_utilization)[i] = device_get_utilization(device);
     
-    nvmlComputeMode_t mode;
-    CHECK_NVML( nvmlDeviceGetComputeMode(device, &mode) );
-    if (mode == NVML_COMPUTEMODE_DEFAULT)
-      strcpy(str, "Default");
-    else if (mode == NVML_COMPUTEMODE_EXCLUSIVE_THREAD)
-      strcpy(str, "Exclusive Thread");
-    else if (mode == NVML_COMPUTEMODE_PROHIBITED)
-      strcpy(str, "Prohibited");
-    else if (mode == NVML_COMPUTEMODE_EXCLUSIVE_PROCESS)
-      strcpy(str, "Exclusive Process");
+    device_get_compute_mode(device);
     SET_STRING_ELT(ret_mode, i, mkChar(str));
   }
   

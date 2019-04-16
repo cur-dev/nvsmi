87254923ca59335c64bf2f4332dc7dc0f5335ad1
Drew Schmidt <wrathematics@gmail.com>
2019-04-15 20:53:35 -0400

    more docs

--- R/smi.r
+++ R/smi.r
@@ -2,6 +2,29 @@
 #' 
 #' An \code{nvidia-smi} clone.
 #' 
+#' @return
+#' A list with 3 elements of class \code{nvidia_smi}:
+#' \itemize{
+#'   \item \code{version}: 
+#'   \item \code{date}: 
+#'   \item The \code{gpus} dataframe: 
+#'   \itemize{
+#'     \item \code{name}: 
+#'     \item \code{busid}: 
+#'     \item \code{persistence_mode}: 
+#'     \item \code{disp}: 
+#'     \item \code{speed}: 
+#'     \item \code{temp}: 
+#'     \item \code{perf}: 
+#'     \item \code{power}: 
+#'     \item \code{power_max}: 
+#'     \item \code{memory_used}: 
+#'     \item \code{memory_total}: 
+#'     \item \code{utilization}: 
+#'     \item \code{compute_mode}: 
+#'   }
+#' }
+#' 
 #' @useDynLib nvsmi R_smi
 #' @export
 smi = function()
